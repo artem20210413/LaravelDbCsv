2fe77342a5d4bca883a517c18ed275d719c599c3
Artem Tishchenko <82469944+artem20210413@users.noreply.github.com>
2023-03-23 11:23:08 +0200

    add csv

---
+++ src/Csv.php
@@ -0,0 +1,40 @@
+<?php
+namespace App;
+
+
+use Illuminate\Support\Facades\DB;
+
+class Csv
+{
+    public function setTableCsv($table, $arrName, $path, $dir, $ignoreFirstRow = true, $separator = ';'): bool
+    {
+        $file = fopen("$dir/$path", 'r');
+        $arr = [];
+        $j = 0;
+
+        while (($line = fgetcsv($file)) !== FALSE) {
+            $row = explode($separator, current($line));
+
+            if ($ignoreFirstRow) {
+                $ignoreFirstRow = false;
+                continue;
+            }
+
+            foreach ($arrName as $i => $value) {
+                $rowValue = strtolower($row[$i]);
+                $arr[$j][$value] = ($rowValue === "null") ? null : $rowValue;
+            }
+            $j++;
+        }
+
+        fclose($file);
+
+        if (!empty($arr)) {
+            DB::table($table)->insert($arr);
+            return true;
+        }
+
+        return false;
+    }
+
+}
